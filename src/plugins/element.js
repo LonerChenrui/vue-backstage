645f3266f738437a80cc379182400b16b4e1c439
LonerChenrui <1823449792@qq.com>
2020-08-18 21:53:59 +0800

    商品分类

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -29,6 +29,7 @@ import {
   Tree,
   Select,
   Option,
+  Cascader
 } from 'element-ui'
 
 Vue.use(Button)
@@ -58,6 +59,7 @@ Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Cascader)
 
 // Message需要全局配置才可使用
 Vue.prototype.$message = Message
--- src/views/cate/categories/categories.vue
+++ src/views/cate/categories/categories.vue
@@ -3,56 +3,82 @@
     <!-- 商品分类面包屑 -->
     <user-breadcrumb :breadcrumbTitle="breadcrumbTitle" />
 
-    <!-- 添加 -->
-    <div>
-      <el-button type="primary" size="small" style="margin:10px 0">添加分类</el-button>
-    </div>
+    <el-card>
+      <!-- 添加 -->
+      <div>
+        <el-button type="primary" size="small" style="margin:10px 0" @click="addGoodsDialog">添加分类</el-button>
+      </div>
 
-    <!-- 商品分类列表 -->
-    <tree-table
-      :data="treeTableDate"
-      :columns="treeTableColumns"
-      :expand-type="false"
-      :selection-type="false"
-      show-index
-    >
-      <!-- 是否有效 -->
-      <template slot="isOk" slot-scope="{row}">
-        <div>
-          <i class="el-icon-success" v-if="row.cat_deleted"></i>
-          <i class="el-icon-error" v-else></i>
-        </div>
-      </template>
+      <!-- 商品分类列表 -->
+      <tree-table
+        :data="treeTableDate"
+        :columns="treeTableColumns"
+        :expand-type="false"
+        :selection-type="false"
+        show-index
+      >
+        <!-- 是否有效 -->
+        <template slot="isOk" slot-scope="{row}">
+          <div>
+            <i class="el-icon-success" v-if="row.cat_deleted"></i>
+            <i class="el-icon-error" v-else></i>
+          </div>
+        </template>
 
-      <!-- 排序 -->
-      <template slot="sort" slot-scope="{row}">
-        <div>
-          <el-tag v-if="row.cat_level == 0" size="mini">标签一</el-tag>
-          <el-tag v-else-if="row.cat_level == 1" type="success" size="mini">标签二</el-tag>
-          <el-tag v-else="row.cat_level == 2" type="warning" size="mini">标签三</el-tag>
-        </div>
-      </template>
+        <!-- 排序 -->
+        <template slot="sort" slot-scope="{row}">
+          <div>
+            <el-tag v-if="row.cat_level == 0" size="mini">标签一</el-tag>
+            <el-tag v-else-if="row.cat_level == 1" type="success" size="mini">标签二</el-tag>
+            <el-tag v-else="row.cat_level == 2" type="warning" size="mini">标签三</el-tag>
+          </div>
+        </template>
 
-      <!-- 操作 -->
-      <template slot="acitve" slot-scope="{row}">
-        <div>
-          <el-button type="primary" size="mini">编辑</el-button>
-          <el-button type="danger" size="mini">删除</el-button>
-        </div>
-      </template>
-    </tree-table>
+        <!-- 操作 -->
+        <template slot="acitve" slot-scope="{row}">
+          <div>
+            <el-button type="primary" size="mini">编辑</el-button>
+            <el-button type="danger" size="mini">删除</el-button>
+          </div>
+        </template>
+      </tree-table>
 
-    <!-- 商品分类分页 -->
-    <div class="categoriesPage">
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="queryInfo.pagenum"
-        :page-size="queryInfo.pagesize"
-        layout="total, prev, pager, next, jumper"
-        :total="total"
-      ></el-pagination>
-    </div>
+      <!-- 商品分类分页 -->
+      <div class="categoriesPage">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-size="queryInfo.pagesize"
+          layout="total, prev, pager, next, jumper"
+          :total="total"
+        ></el-pagination>
+      </div>
+    </el-card>
+
+    <!-- 添加商品分类弹框 -->
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="60%" :before-close="handleClose">
+      <div>
+        <el-form
+          :model="addGoodsData"
+          :rules="addGoodsRules"
+          ref="addGoodsRuleForm"
+          label-width="100px"
+        >
+          <el-form-item label="分类名称" prop="cat_name">
+            <el-input v-model="addGoodsData.cat_name"></el-input>
+          </el-form-item>
+
+          <el-form-item label="父级分类">
+            <el-cascader v-model="pitchOnValue" :options="parentTypeData" :props="cascaderProps"></el-cascader>
+          </el-form-item>
+        </el-form>
+      </div>
+      <span slot="footer">
+        <el-button @click="handleClose">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -93,11 +119,41 @@ export default {
       // 初始化查询列表条件
       queryInfo: {
         pagenum: 1,
-        pagesize: 2,
+        pagesize: 5,
         type: 3,
       },
       // 总条数
       total: 0,
+      // 添加分类弹框
+      dialogVisible: false,
+      // 添加分类From数据
+      addGoodsData: {
+        // 分类名称
+        cat_name: "",
+      },
+      // 添加分类From验证规则
+      addGoodsRules: {
+        cat_name: [
+          { required: true, message: "请输入分类名称", trigger: "blur" },
+          { min: 1, max: 6, message: "长度在 1 到 6 个字符", trigger: "blur" },
+        ],
+      },
+      // 父级分类选中项绑定值
+      pitchOnValue: [],
+      // 父级分类list
+      parentTypeData: [],
+      // 级联选择器配置项
+      cascaderProps: {
+        // 触发子级的方式
+        expandTrigger: "hover",
+        // 对应的值
+        value: "cat_pid",
+        // 显示的文本
+        label: "cat_name",
+        // 哪个字段用于展示子层级
+        children: "children",
+        // checkStrictly: true,
+      },
     };
   },
   components: {
@@ -125,9 +181,25 @@ export default {
     },
     // 当前页
     handleCurrentChange(val) {
-      console.log(val)
+      console.log(val);
       this.queryInfo.pagenum = val;
       this.getCateGoriesList();
+    },
+    // 显示商品分类弹框
+    addGoodsDialog() {
+      getCateGoriesList({ type: 2 }).then((res) => {
+        console.log(res);
+        if (res.meta.status !== 200) {
+          return this.$message.error(res.meta.msg);
+        }
+
+        this.parentTypeData = res.data;
+      });
+      this.dialogVisible = true;
+    },
+    // 关闭商品分类弹框
+    handleClose() {
+      this.dialogVisible = false;
     },
   },
 };
@@ -135,5 +207,13 @@ export default {
 <style scoped lang="less">
 .categoriesPage {
   margin-top: 20px;
+}
+.el-cascader {
+  width: 100%;
+}
+</style>
+<style lang="less">
+.el-popper .el-cascader-panel {
+  max-height: 500px !important;
 }
 </style>
