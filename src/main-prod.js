d40d034d518fb48f641c9aeec46d5c4488cd5c4d
LonerChenrui <1823449792@qq.com>
2020-09-09 18:11:46 +0800

    修改打包入口文件

---
+++ src/main-prod.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/display.css';
+
+import '@/assets/css/base.css'
+import animated from 'animate.css'
+Vue.use(animated)
+
+// 引入自定义过滤器
+import "@/common/js/filters.js";
+
+// 树结构table插件
+import ZkTable from 'vue-table-with-tree-grid'
+Vue.component('tree-table', ZkTable)
+
+// 富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.use(VueQuillEditor, /* { default global options } */)
+
+// 省市县三级联动
+import Distpicker from 'v-distpicker'
+Vue.component('v-distpicker', Distpicker)
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,18 @@ module.exports = {
         views: "@/views"
       }
     }
+  },
+  // 修改webpack打包的入口文件。需要在根目录建两个对应入口js文件
+  chainWebpack: config => {
+    //生产环境   
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')    
+    })
+
+    //开发环境
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')     
+    })
   }
 }
 
