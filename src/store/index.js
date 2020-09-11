5d482c2739948b36ba59b43d5bed4ea3a7436ffa
LonerChenrui <1823449792@qq.com>
2020-09-11 16:29:36 +0800

    数据持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// 1.安装Vuex持久化插件并且引入插件
+import createPersistedState from "vuex-persistedstate";
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 持久化测试
+    numState: 1,
+    // 非持久化测试
+    noState: 1
   },
+  getters: {},
   mutations: {
+    setNumState(state,payload) {
+      
+      state.numState = state.numState * 2;
+
+      state.noState = state.noState * 2;
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  // 2. 在store中调用
+  plugins: [createPersistedState({
+    // 默认存储到localStorage，想要存储到sessionStorage，通过storage属性
+    storage: window.sessionStorage,
+    // vuex-persistedstate默认持久化所有state，通过reducer属性可指定需要持久化的state
+    reducer(state) {
+      return {
+        // 只持久化state中的numState状态
+        numState: state.numState
+      }
+    }
+  })],
 })
--- src/views/home/children/HomeMain.vue
+++ src/views/home/children/HomeMain.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="home-main">
-    hello world
+    <div>hello world</div>
+    <button @click="setNumState(2)">修改</button>
+    <div>从store中取持久化值：{{numState}}</div>
+    <div>从store中取非持久化值：{{noState}}</div>
   </div>
 </template>
 
 <script>
+import { mapMutations, mapState } from "vuex";
 export default {
   name: "home-main",
   data() {
     return {
-    }
+
+    };
   },
-  components:{ },
-  props:{ },
-}
+  components: {},
+  props: {},
+  computed: {
+    ...mapState(['numState','noState']),
+  },
+  methods: {
+    ...mapMutations(['setNumState'])
+
+  }
+};
 </script>
 <style scoped>
 </style>
